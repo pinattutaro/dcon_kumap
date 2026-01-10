1ab1a265293b4c513cdb1078fe56d1507f781bb0
pinattutaro <pinattutaro@gmail.com>
2026-01-11 02:20:17 +0900

    update

--- raspi.js
+++ raspi.js
@@ -1,3 +1,4 @@
+// server urlがgithub上に保管しているので、ここから取得すること。
 const getURL = async () => {
     let url = "";
     try {
@@ -10,9 +11,9 @@ const getURL = async () => {
 }
 
 const main = async () => {
-    const lat = 0;
-    const lon = 0;
-    const imgBuffer = require("fs").readFileSync(__dirname + "/image.png");
+    const lat = 0; // 緯度
+    const lon = 0; // 経度
+    const imgBuffer = require("fs").readFileSync(__dirname + "/image.png"); // 画像ファイル読み込み　ここに熊の画像を入れる
     const imgBlob = new Blob([imgBuffer], { type: 'image/png' });
 
     const formData = new FormData();
